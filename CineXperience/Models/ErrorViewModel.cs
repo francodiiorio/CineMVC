317ec94255f8cbf95227e88b287ba4a5c25a08cb
franco di iorio <franco.diiorio5@gmail.com>
2023-06-28 21:13:58 -0300

    Add project files.

---
+++ CineXperience/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace CineXperience.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
